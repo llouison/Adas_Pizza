f58b75f7771fa4d1300cd834e53382a747e76fb9
lisa louison <lisa.louison@gmail.com>
2017-04-27 21:24:06 -0400

    refactored routes

--- adas-pizza-2/routes/pizzaRoutes.js
+++ adas-pizza-2/routes/pizzaRoutes.js
@@ -1,35 +1,18 @@
 const express = require('express');
+const controller = require('../controllers/pizzaController');
+
 const pizzaRoutes = express.Router();
 
-const pizzaData = require('../db/pizza');
+pizzaRoutes.get('/', controller.index);
 
-pizzaRoutes.get('/', function(req, res) {
-  res.render('pizza/pizza-index', {
-    documentTitle: 'Ada\'s Pizza!',
-    pizzaData: pizzaData,
-  });
-});
-
-pizzaRoutes.get('/add', function(req, res) {
+pizzaRoutes.get('/add', (req, res) => {
   res.render('pizza/pizza-add', {
     documentTitle: 'Ada\'s Pizza!'
   });
 });
 
-pizzaRoutes.get('/:id', function(req, res) {
-  const id = req.params.id;
-  res.render('pizza/pizza-single', {
-    documentTitle: 'Ada\'s Pizza!',
-    onePizza: pizzaData[id],
-  });
-});
+pizzaRoutes.get('/:id', controller.show);
 
-pizzaRoutes.post('/', function(req, res) {
-  pizzaData.push({
-    flavor: req.body.flavor,
-    location: req.body.location
-  });
-  res.redirect('/pizza');
-});
+pizzaRoutes.post('/', controller.create);
 
 module.exports = pizzaRoutes;
